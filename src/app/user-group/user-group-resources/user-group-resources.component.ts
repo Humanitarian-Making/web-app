3a69389029beaaecb44f7d2f862f7c56f92c2a5a
patricklrollinson@gmail.com <patrick@futuremedia.co.za>
2020-06-09 09:37:36 +0100

    Create user group resources component

---
+++ src/app/user-group/user-group-resources/user-group-resources.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-group-resources',
+  templateUrl: './user-group-resources.component.html',
+  styleUrls: ['./user-group-resources.component.scss']
+})
+export class UserGroupResourcesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
