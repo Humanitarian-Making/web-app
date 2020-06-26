f3dbb010259356f5f0cbbbbd84a2fbfcdcaeecc1
patricklrollinson@gmail.com <patrick@futuremedia.co.za>
2020-06-26 12:06:54 +0100

    Minor Home Page changes

--- src/app/core/components/home/home-sections/home-title-section/home-title-section.component.ts
+++ src/app/core/components/home/home-sections/home-title-section/home-title-section.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./../../home.component.scss']
 })
 export class HomeTitleSectionComponent implements OnInit {
-
+  
   constructor(private router: Router) { }
 
   ngOnInit(): void {
